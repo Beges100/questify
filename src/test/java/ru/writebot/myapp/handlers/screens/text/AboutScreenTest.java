13e223fec27d0a3868b2c1ac5bc9deb8ecb76452
Konstantin Mushenko <beges200@gmail.com>
2024-04-09 12:35:34 +0300

    Рефакторинг. Заменил @Data на геттеры и сеттеры + equals and hashcode

---
+++ src/test/java/ru/writebot/myapp/handlers/screens/text/AboutScreenTest.java
@@ -0,0 +1,13 @@
+package ru.writebot.myapp.handlers.screens.text;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AboutScreenTest {
+
+    @Test
+    public void testFirst() {
+        assertEquals(2, 1 + 1);
+    }
+}
